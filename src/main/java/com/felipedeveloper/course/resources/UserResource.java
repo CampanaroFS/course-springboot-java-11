db48dd7c799f15eca4cc3e685c76558e720c7bca
Felipe Campanaro <felipe.s.campanaro@gmail.com>
2020-04-26 11:29:09 -0300

    H2 database, test profile, JPA

--- src/main/java/com/felipedeveloper/course/resources/UserResource.java
+++ src/main/java/com/felipedeveloper/course/resources/UserResource.java
@@ -12,7 +12,7 @@ import com.felipedeveloper.course.entities.User;
 public class UserResource {
 	
 	@GetMapping
-	public ResponseEntity<User> findAll(){
+	public ResponseEntity<User> findAll() {
 		User u = new User(1L, "Maria", "maria@gmail.com", "99999999", "123456");
 		return ResponseEntity.ok().body(u);
 	}
